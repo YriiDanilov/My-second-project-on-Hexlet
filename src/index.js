b0c1232ac7a361c59f5515a58e50a80de02941e1
YriiDanilov <relantii@mail.ru>
2023-06-06 23:42:57 +0300

    fit err

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 export default (filepath1, filepath2) => {
     const readFile1 = readFileSync(filepath1, 'utf-8');
     const parse1 = JSON.parse(readFile1);
-    console.log(parse);
+    console.log(parse1);
     const readFile2 = readFileSync(filepath2, 'utf-8');
     const parse2 = JSON.parse(readFile2);
     console.log(parse2);
