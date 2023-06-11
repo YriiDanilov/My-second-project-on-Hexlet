f6f3900e319f2c981985dbad46fd151707c6a2b3
YriiDanilov <relantii@mail.ru>
2023-06-11 15:51:52 +0300

    finish step 3

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const gendiff = (filePath1, filePath2) => {
   const firstObj = getParseData(filePath1);
   const secondObj = getParseData(filePath2);
   const keys = getSortedKeys(firstObj, secondObj);
-  const difference = keys.reduce((acc, key) => {
+  const conditions = keys.reduce((acc, key) => {
     if (!_.has(firstObj, key)) {
       acc[`+ ${key}`] = secondObj[key];
     } else if (!_.has(firstObj, key)) {
@@ -26,7 +26,7 @@ const gendiff = (filePath1, filePath2) => {
     }
     return acc;
   }, {});
-  const convertObjInString = JSON.stringify(difference, null, '  ');
+  const convertObjInString = JSON.stringify(conditions, null, '  ');
   return convertObjInString.replace(/"/gi, '').replace(/,/gi, '');
 };
 
--- src/readfile.js
+++ src/readfile.js
@@ -3,6 +3,8 @@ import path from 'path';
 import process from 'process';
 
 export default (filePath) => {
+  //Óonst extension = path.extname(filePath);
+  //const basename = path.basename(filePath); 
   const absolutePath = path.resolve(process.cwd(), filePath);
   const readJson = readFileSync(absolutePath, 'utf-8');
   const result = JSON.parse(readJson);
