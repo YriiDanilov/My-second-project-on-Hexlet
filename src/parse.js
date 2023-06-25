930e8284b1ba7b6f55b8d3e9278b3a1d6a22f84e
YriiDanilov <relantii@mail.ru>
2023-06-25 18:58:31 +0300

    try fix

--- src/parse.js
+++ src/parse.js
@@ -1,10 +1,10 @@
-import YAML from 'js-yaml';
+import YAML from 'yaml';
 
 export default (filepath, ext) => {
     switch (ext) {
         case 'json': JSON.parse(filepath);
-        case 'yaml': YAML.load(filepath);
-        case 'yml': YAML.load(filepath);
+        case 'yaml': YAML.parse(filepath);
+        case 'yml': YAML.parse(filepath);
         default:
             console.log(`Unknown format ${ext}!`);
     };
